9228ef922ca65e56f883dcb37d44d64f84a325dc
Jon Masropian <jon.masropian@gmail.com>
2026-05-07 19:38:27 -0500

    modified JonMasropian.jsx

--- JonMasropian.jsx
+++ JonMasropian.jsx
@@ -439,3 +439,62 @@ function Cart() {
         </div>
     );
 }
+
+
+// useMemo - Caching an exspensive calculation
+function ProductFilter( {products} ) {
+    const [search, setSearch] = useState("");
+    const [color, setColor] = useState("blue");
+    
+    const filtered = useMemo( () => {
+        console.log("Filtering products...");
+        return products.filter( p => p.name.toLowerCase().icludes(search.toLocaleLowerCaes()));
+    }, [products, search]);
+    
+    return (
+        <div>
+            <input
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder="Search Products"
+            />
+            <input
+                value={color}
+                onChange={(e) => setColor((e.target.value))}
+                placeholder="Favorite color (does NOT re-filter)"
+            />
+            <ul>
+                {filtered.map( p => <li key={p.id}>{p.name}</li>)}
+            </ul>
+        </div>
+    );
+}
+
+
+function ProductFilter( {products} ) {
+    const [search, setSearch] = useState("");
+    const [color, setColor] = useState("blue");
+    
+    const filtered = useMemo( () => {
+        console.log("Filtering products...");
+        return products.filter( p => p.name.toLowerCase().includes(search.toLocaleLowerCase()));
+    }, [products, search]);
+    
+    return (
+        <div>
+            <input
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder="Search Products"
+            />
+            <input
+                value={color}
+                onChange={(e) => setColor((e.target.value))}
+                placeholder="Favorite color (does NOT re-filter)"
+            />
+            <ul>
+                {filtered.map( p => <li key={p.id}>{p.name}</li>)}
+            </ul>
+        </div>
+    );
+}
